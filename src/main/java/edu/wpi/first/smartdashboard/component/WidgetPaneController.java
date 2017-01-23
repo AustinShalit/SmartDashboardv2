de0f4c4df480fc644b305ebc0bf56de273955b1f
Brad Miller <brad@bradhouse.com>
2017-01-22 21:32:11 -0500

    Added a LabeledTextWidget - curious what it would look like

--- src/main/java/edu/wpi/first/smartdashboard/component/WidgetPaneController.java
+++ src/main/java/edu/wpi/first/smartdashboard/component/WidgetPaneController.java
@@ -2,7 +2,10 @@ package edu.wpi.first.smartdashboard.component;
 
 import edu.wpi.first.smartdashboard.widget.ConnectionIndicator;
 
+import edu.wpi.first.smartdashboard.widget.LabeledTextField;
+import edu.wpi.first.smartdashboard.widget.ResizableAndDraggableWidget;
 import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 public class WidgetPaneController {
@@ -12,11 +15,18 @@ public class WidgetPaneController {
 
   @FXML
   void initialize() {
+    int yValue = 0;
     assert pane != null : "fx:id=\"pane\" was not injected: check your FXML file 'WidgetPaneController.fxml'.";
 
-    ConnectionIndicator test = new ConnectionIndicator();
+    ResizableAndDraggableWidget connectionIndicator = new ConnectionIndicator();
+    ResizableAndDraggableWidget labeledText = new LabeledTextField("X Value");
 
-    pane.getChildren().addAll(test);
+    pane.getChildren().add(labeledText);
+    labeledText.setLayoutY(yValue);
+    yValue += labeledText.getHeight() + 5;
+
+    pane.getChildren().add(connectionIndicator);
+    connectionIndicator.setLayoutY(yValue);
   }
 
 }
